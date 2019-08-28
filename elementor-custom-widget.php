72bfd673b0196e5b8664fbcbdd3f1910a46dfc19
ISN <aishans@gmail.com>
2019-08-28 18:34:56 +0545

    Update elementor-custom-widget.php

--- elementor-custom-widget.php
+++ elementor-custom-widget.php
@@ -5,7 +5,7 @@
  */
 if ( ! defined( 'ABSPATH' ) ) exit;
 define('ECW_PLUGIN_PLUGIN_PATH', plugin_dir_path( __FILE__ ));
-
+define( 'ECW_PLUGIN_DIR_URL', plugin_dir_url( __FILE__ ) );
 
 // plug it in
 add_action('plugins_loaded', 'ecw_require_files');
@@ -17,5 +17,5 @@ function ecw_require_files() {
 // enqueue your custom style/script as your requirements
 // add_action( 'wp_enqueue_scripts', 'ecw_enqueue_styles', 25);
 function ecw_enqueue_styles() {
-    wp_enqueue_style( 'elementor-custom-widget-editor', ECW_PLUGIN_PLUGIN_PATH( __FILE__ ) . 'assets/css/editor.css');
+    wp_enqueue_style( 'elementor-custom-widget-editor', ECW_PLUGIN_DIR_URL . 'assets/css/editor.css');
 }
